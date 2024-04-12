3bae9b3d86e25877a21d47ce9ff825ededd9b9bb
Anjanikumar-G <anjanikumar14333@gmail.com>
2024-04-12 18:00:50 +0530

    questions

---
+++ script.js
@@ -0,0 +1,27 @@
+// 1. What are the different data types present in javascript?
+
+// ans: To know the type of a JavaScript variable, we can use the typeof operator.
+//     1. Primitive types
+//       .String
+//       .Number
+//       .BigInt
+//       .Boolean
+//       .Undefined
+//       .Null
+//       .Symbol
+//     2. Non-primitive types
+//        Primitive data types can store only a single value. To store multiple and complex values, non-primitive data types are used.
+//        Object - Used to store collection of data
+
+// 2. Explain Hoisting in javascript.
+
+// ans: Hoisting is the default behaviour of javascript where all the variable and function declarations are moved on top.
+//     example :
+//             hoistedVariable = 3;
+//             console.log(hoistedVariable); // outputs 3 even when the variable is declared after it is initialized
+//             var hoistedVariable;
+
+// 3. Why do we use the word “debugger” in javascript?
+
+// ans: The debugger for the browser must be activated in order to debug the code. Built-in debuggers may be switched on and off,
+//      requiring the user to report faults. The remaining section of the code should stop execution before moving on to the next line while debugging.
