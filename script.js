99b6221dae0ee68ec2dfcf7c0b392ced54d20952
Anjanikumar-G <anjanikumar14333@gmail.com>
2024-04-13 22:21:40 +0530

    Latest Questions

--- script.js
+++ script.js
@@ -25,3 +25,19 @@
 
 // ans: The debugger for the browser must be activated in order to debug the code. Built-in debuggers may be switched on and off,
 //      requiring the user to report faults. The remaining section of the code should stop execution before moving on to the next line while debugging.
+
+// 4. Difference between “ == “ and “ === “ operators.
+
+// ans: Both are comparison operators. The difference between both the operators is that “==” is used to compare values
+//       whereas, “ === “ is used to compare both values and types.
+
+// 5.Difference between var and let keyword in javascript.
+
+// ans: Variables declared by let are only available inside the block where they’re defined.
+//      Variables declared by var are available throughout the function in which they’re declared.
+
+// 6. What is NaN property in JavaScript?
+
+// ans: NaN property represents the “Not-a-Number” value. It indicates a value that is not a legal number.
+//      typeof of NaN will return a Number.
+//      To check if a value is NaN, we use the isNaN() function,
