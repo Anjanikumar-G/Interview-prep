11da012b652b1c0c089dccb8de17b8457d06b968
Anjanikumar-G <anjanikumar14333@gmail.com>
2024-04-19 15:30:13 +0530

    19/04/2024

--- script.js
+++ script.js
@@ -110,3 +110,99 @@
 //      (function(){
 //      // Do something;
 //      })();
+
+// 13.Explain “this” keyword
+
+// ans:The “this” keyword refers to the object that the function is a property of.
+//     The value of the “this” keyword will always depend on the object that is invoking the function
+// Example :
+
+// var obj = {
+//     name:  "vivek",
+//     getName: function(){
+//     console.log(this.name);
+//   }
+// }
+
+// obj.getName()
+
+// 14. Explain call(), apply() and, bind() methods
+
+// ans: 1. call():
+//     It’s a predefined method in javascript.
+//     This method invokes a method (function) by specifying the owner object.
+// Example :
+// var person = {
+//   age: 23,
+//   getAge: function(){
+//     return this.age;
+//   }
+// }
+// var person2 = {age:  54};
+// person.getAge.call(person2);
+// // Returns 54
+//     2.apply()
+//     The apply method is similar to the call() method. The only difference is that,
+//     call() method takes arguments separately whereas, apply() method takes arguments as an array.
+// Example :
+// function saySomething(message){
+//   return this.name + " is " + message;
+//   }
+// var person4 = {name:  "John"};
+// saySomething.apply(person4, ["awesome"]);
+// // Returns "John is awesome"
+//   3. bind()
+//   This method returns a new function, where the value of “this” keyword will be bound to the owner object, which is provided as a parameter.
+// Example :
+// var bikeDetails = {
+//     displayDetails: function(registrationNumber,brandName){
+//     return this.name+ " , "+ "bike details: "+ registrationNumber + " , " + brandName;
+//   }
+// }
+// var person1 = {name:  "Vivek"};
+// var detailsOfPerson1 = bikeDetails.displayDetails.bind(person1, "TS0122", "Bullet");
+// // Binds the displayDetails function to the person1 object
+// detailsOfPerson1();
+// //Returns Vivek, bike details: TS0122, Bullet
+
+// 15.Explain Scope and Scope Chain in javascript
+
+// ans:Scope in JS determines the accessibility of variables and functions at various parts of one’s code.
+//     In general terms, the scope will let us know at a given part of code, what are variables and functions we can or cannot access.
+// There are three types of scopes in JS:
+//     1.Global Scope
+//     2.Local or Function Scope
+//     3.Block Scope
+
+//    1. Global Scope: Variables or functions declared in the global namespace have global scope, which means all the variables and functions having global scope can be accessed from anywhere inside the code.
+
+// var globalVariable = "Hello world";
+// function sendMessage(){
+//   return globalVariable; // can access globalVariable since it's written in global space
+// }
+// function sendMessage2(){
+//   return sendMessage(); // Can access sendMessage function since it's written in global space
+// }
+// sendMessage2();  // Returns “Hello world”
+
+//    2.Function Scope: Any variables or functions declared inside a function have local/function scope, which means that all the variables and functions declared inside a function, can be accessed from within the function and not outside of it.
+
+// function awesomeFunction(){
+//   var a = 2;
+//   var multiplyBy2 = function(){
+//     console.log(a*2); // Can access variable "a" since a and multiplyBy2 both are written inside the same function
+//   }
+// }
+// console.log(a); // Throws reference error since a is written in local scope and cannot be accessed outside
+// multiplyBy2(); // Throws reference error since multiplyBy2 is written in local scope
+
+//    3.Block Scope: Block scope is related to the variables declared using let and const. Variables declared with var do not have block scope. Block scope tells us that any variable declared inside a block { }, can be accessed only inside that block and cannot be accessed outside of it.
+
+// {
+//   let x = 45;
+// }
+// console.log(x); // Gives reference error since x cannot be accessed outside of the block
+// for(let i=0; i<2; i++){
+//   // do something
+// }
+// console.log(i); // Gives reference error since i cannot be accessed outside of the for loop block
